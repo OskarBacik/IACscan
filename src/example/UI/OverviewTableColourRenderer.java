f30860e24e2a625d0beae922ac1b34eb11843cb2
Oskar <c1010801@ncl.ac.uk>
2024-03-03 20:26:15 +0000

    Added more response codes to colour render

--- src/example/UI/OverviewTableColourRenderer.java
+++ src/example/UI/OverviewTableColourRenderer.java
@@ -3,6 +3,9 @@ package example.UI;
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 // Custom colour renderer for Overview Table
 class OverviewTableColourRenderer extends DefaultTableCellRenderer {
@@ -10,18 +13,24 @@ class OverviewTableColourRenderer extends DefaultTableCellRenderer {
   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
     Component cellComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
-    Integer responseCode;
-    // colour code each cell based on the response code TODO: add more response codes
+    int responseCode;
+    List<Integer> greenList = Arrays.asList(200,201,202);
+    List<Integer> orangeList = Arrays.asList(300,301,302,304,307,308);
+    List<Integer> redList = Arrays.asList(400,401,402,403);
+
+    // colour code each cell based on the response code
     Object cell = table.getValueAt(row, column);
     try {
       responseCode = Integer.parseInt((String) cell);
     } catch (NumberFormatException exception) {
       responseCode = 0;
     }
-    if (responseCode == 200) {
+    if (greenList.contains(responseCode)) {
       cellComponent.setBackground(Color.GREEN);
-    } else if (responseCode == 403) {
+    } else if (redList.contains(responseCode)) {
       cellComponent.setBackground(Color.RED);
+    } else if (orangeList.contains(responseCode)) {
+      cellComponent.setBackground(Color.ORANGE);
     } else if (responseCode == 404) {
       cellComponent.setBackground(Color.GRAY);
     } else {
